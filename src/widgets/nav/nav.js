94e89973c0a34bf4cc884b1c046f1637ae2f6609
AlenaVP Legion <poprugo@gmail.com>
2026-09-19 00:37:12 +0200

    feat: add widget header

--- src/widgets/nav/nav.js
+++ src/widgets/nav/nav.js
@@ -1,8 +1,8 @@
 import navTemplate from './nav.html?raw';
 import './nav.scss';
+import { createElementFromTemplate } from '../../shared/lib/dom';
 
-export function createNav(mountSelector) {
-  const mount = document.querySelector(mountSelector);
-  if (!mount) return;
-  mount.innerHTML = navTemplate;
+export function createNav() {
+  const navElement = createElementFromTemplate(navTemplate);
+  return navElement;
 }
