426bd57eac433df2fa7614c6bd0b0d84f786063c
AlenaVP Legion <poprugo@gmail.com>
2026-09-19 19:43:09 +0200

    feat: add a page home

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import 'modern-normalize';
 import './app/styles/main.scss';
+import { createHeader } from './widgets/header/header';
+import { createHomePage } from './pages/home/home';
+import { createFooter } from './widgets/footer/footer';
+import { mountChild } from './shared/lib/dom';
 
-document.querySelector('#app').innerHTML = `<span>template</span>`;
+mountChild(document, '#header', createHeader());
+mountChild(document, '#app', createHomePage());
+mountChild(document, '#footer', createFooter());
